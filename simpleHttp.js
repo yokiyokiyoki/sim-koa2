e9ebffb17f804d5199d3b1366c09c7a1e465a486
youjie <youjie@hudongpai.com>
2018-04-19 14:57:56 +0800

    初始化提交

--- simpleHttp.js
+++ simpleHttp.js
@@ -1,1 +1,10 @@
 //使用原生http模块，为客户端提供响应
+const http = require("http");
+const server = http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end("hi yoki");
+});
+//监听3000端口
+server.listen(3000, () => {
+  console.log("正在监听3000端口");
+});
