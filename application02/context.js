3fad3239e262e763e0f3fdc107ae69ff1d16dd45
youjie <youjie@hudongpai.com>
2018-04-19 16:18:14 +0800

    修改上下文

--- application02/context.js
+++ application02/context.js
@@ -1,6 +1,22 @@
 //context主要是一些常用方法的代理，这样我们在request.js里面才可以this.query这样写
+//通过context.query直接代理了context.request.query，context.body和context.status代理了context.response.body与context.response.status。而context.request，context.response则会在application.js中挂载
 const context = {
   get query() {
     return this.request.query;
+  },
+  get body() {
+    return this.response.body;
+  },
+
+  set body(data) {
+    this.response.body = data;
+  },
+
+  get status() {
+    return this.response.status;
+  },
+
+  set status(statusCode) {
+    this.response.status = statusCode;
   }
 };
