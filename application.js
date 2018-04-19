184fb9f660b1959933f780db3aa617883c9e8937
youjie <youjie@hudongpai.com>
2018-04-19 15:25:04 +0800

    简单封装完成

--- application.js
+++ application.js
@@ -6,9 +6,21 @@ class App {
     //外面传进来的回调函数
     this.callbackFunc;
   }
-  //   开启http server
+  //开启http server,传进来callback
   listen(...args) {
-    const server = http.createServer(this.callbackFunc);
+    const server = http.createServer(this.callback());
     server.listen(...args);
   }
+  //挂载回调函数
+  use(fn) {
+    this.callbackFunc = fn;
+  }
+  //获取传进来所需的callback
+  callback() {
+    return (req, res) => {
+      this.callbackFunc(req, res);
+    };
+  }
 }
+//这样外面引用可以用require
+module.exports = App;
