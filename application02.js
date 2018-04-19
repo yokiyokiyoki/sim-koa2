4b643381e37095bbbe4d14712bcf87c8a5ff5326
youjie <youjie@hudongpai.com>
2018-04-19 15:53:03 +0800

    第二个版本

--- application02.js
+++ application02.js
@@ -1,2 +1,4 @@
 // koa有三个重要对象，request，response，context
-//封装node原生的request对象，封装node原生的response对象，context是回调函数上下文
+//封装node原生的request对象，封装node原生的response对象
+//context是回调函数上下文,挂在了koa的req和res对象
+//通过对象getter和setter来封装
