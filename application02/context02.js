b5db2f903e33dfae3b7e5b41aca609e7df38e970
youjie <youjie@hudongpai.com>
2018-04-19 16:29:37 +0800

    设置一下getter

--- application02/context02.js
+++ application02/context02.js
@@ -1,2 +1,15 @@
 //一个个写有点笨，这里封装一下，再通过数组foreach进去
 const context = {};
+
+//为context设置一下setter
+function delegateSet(property, name) {
+  context.__defineSetter__(name, val => {
+    this[property][name] = val;
+  });
+}
+//设置一下getter
+function delegateGet(property, name) {
+  context.__defineGetter__(name, val => {
+    return this[property][name];
+  });
+}
