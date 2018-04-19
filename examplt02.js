3e85231a037db7fd13f42bf5db640883532f9ea2
youjie <youjie@hudongpai.com>
2018-04-19 19:26:07 +0800

    初始化

---
+++ examplt02.js
@@ -0,0 +1,11 @@
+const koa = require("./application02/application");
+const app = new koa();
+
+//这里是async 函数,然后then里面执行抛出body内容出去
+app.use(async ctx => {
+  ctx.body = "hi" + ctx.query.name;
+});
+
+app.listen(3000, () => {
+  console.log("正在监听3000端口");
+});
