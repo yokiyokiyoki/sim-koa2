95356a9cdd46cb9a2095a5e1eac7ac0272a5b5b7
youjie <youjie@hudongpai.com>
2018-04-19 15:33:15 +0800

    初始化执行

--- example.js
+++ example.js
@@ -1,1 +1,11 @@
-let koa = require("./application");
+const koa = require("./application");
+const app = new koa();
+
+app.use((req, res) => {
+  res.writeHead(200);
+  res.end("hi yoki");
+});
+
+app.listen(3000, () => {
+  console.log("正在监听3000端口");
+});
